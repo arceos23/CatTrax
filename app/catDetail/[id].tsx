9daf7f785882696026841ff0149da43d2b0b0c1f
Shane Arceo <arceos@seas.upenn.edu>
2023-05-23 13:10:30 -0400

    added remove individual cat from specific cat page

--- app/catDetail/[id].tsx
+++ app/catDetail/[id].tsx
@@ -1,23 +1,35 @@
 import { useContext, useEffect } from 'react';
 import { SafeAreaView } from 'react-native';
-import { Stack, useLocalSearchParams } from 'expo-router';
+import { Stack, useLocalSearchParams, useRouter } from 'expo-router';
 import styles from './CatDetailStyle';
 import CatDetail from '../../components/catDetail/CatDetail';
 import CatsContext from '../../hooks/CatsContext';
+import { Button } from 'react-native-paper';
 
 const catDetail = () => {
+  const router = useRouter();
   const { cats, setCats } = useContext(CatsContext);
   const { id } = useLocalSearchParams();
 
   const cat = cats.filter((cat) => {
     return cat.id === id;
   })[0];
-  return (
+  return cat ? (
     <SafeAreaView style={styles.container}>
       <Stack.Screen options={{ title: cat.name }} />
       <CatDetail {...cat}></CatDetail>
+      <Button>Edit cat</Button>
+      <Button
+        onPress={() => {
+          router.push('/home');
+          setCats(cats.filter((cat) => cat.id !== id));
+        }}
+      >
+        Remove cat
+      </Button>
+      <Button>Favorite cat</Button>
     </SafeAreaView>
-  );
+  ) : null;
 };
 
 export default catDetail;
--- components/catDetail/CatDetail.tsx
+++ components/catDetail/CatDetail.tsx
@@ -1,5 +1,5 @@
-import { Text, Image, ImageSourcePropType } from 'react-native';
-import { Surface, Card, Button } from 'react-native-paper';
+import { Text } from 'react-native';
+import { Card, Button } from 'react-native-paper';
 import styles from './CatDetailStyle';
 
 interface CatDetailProps {
@@ -13,25 +13,21 @@ interface CatDetailProps {
 }
 
 const CatDetail = ({ name, breed, age, favFoods, description, photo }: CatDetailProps) => {
-  console.log(name, breed, age, favFoods, description, photo);
   return (
     <Card>
-      {/* <Card.Title title="Card Title" subtitle="Card Subtitle" left={LeftContent} /> */}
-      {/* <Card.Title title={`Name: ${name}`} subtitle={`Breed: ${breed}`} /> */}
       <Card.Content>
         <Text>{`Name: ${name}`}</Text>
         <Text>{`Breed: ${breed}`}</Text>
         <Text>{`Age: ${age}`}</Text>
         <Text>{`Favorite foods: ${favFoods}`}</Text>
         <Text>{`Description: ${description}`}</Text>
-        {/* <Image source={{ uri: photo }} style={styles.photo} accessibilityIgnoresInvertColors /> */}
       </Card.Content>
       <Card.Cover source={{ uri: photo }} />
-      <Card.Actions>
+      {/* <Card.Actions>
         <Button>Edit cat</Button>
-        <Button>Remove cat</Button>
+        <Button onPress={setCats(cats.filter((cat) => cat.id === id))}>Remove cat</Button>
         <Button>Favorite cat</Button>
-      </Card.Actions>
+      </Card.Actions> */}
     </Card>
   );
 };
