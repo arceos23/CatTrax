61ad2cf483fc3dd22083f013782b982fb43b8dda
Shane Arceo <arceos@seas.upenn.edu>
2023-05-24 01:54:32 -0400

    updated edit cat margins

--- app/editCat/editCatStyle.ts
+++ app/editCat/editCatStyle.ts
@@ -1,9 +1,11 @@
 import { Platform, StatusBar, StyleSheet } from 'react-native';
+import { SPACING } from '../../constants/constants';
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
     paddingTop: Platform.OS === 'android' ? StatusBar.currentHeight : 0,
+    margin: SPACING,
   },
 });
 
